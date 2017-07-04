9afac986c8772de9a68026de7135f8027876a66f
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-07-04 16:52:31 -0700

    Configured routes for subdomain api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+    # To organize controllers by namespace, format Json, subdomain api, path: "/" ex: api.taskmanager.com/
+
+
+  end
+
 end
