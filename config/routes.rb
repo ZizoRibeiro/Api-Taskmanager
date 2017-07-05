972bef35267c260f1422bd1961a1377613e0f7c8
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-07-04 18:23:17 -0700

    Added Api Version Constraint class for versioning routes and domains

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
     # To organize controllers by namespace, format Json, subdomain api, path: "/" ex: api.taskmanager.com/
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+      resources :tasks
+    end
 
+    namespace :v2, path: "/", constraints: ApiVersionConstraint.new(version: 2) do
+      resources :tasks
+    end
 
   end
 
