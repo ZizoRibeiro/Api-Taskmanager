bee292f9d1e493ee031bd4a8be814d361118908c
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-07-06 14:05:41 -0700

    Added show action and test for user controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
     # To organize controllers by namespace, format Json, subdomain api, path: "/" ex: api.taskmanager.com/
     namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
-      resources :tasks
+      resources :users, only: [:show]
     end
 
-    namespace :v2, path: "/", constraints: ApiVersionConstraint.new(version: 2) do
-      resources :tasks
-    end
+    # namespace :v2, path: "/", constraints: ApiVersionConstraint.new(version: 2) do
+    #   resources :tasks
+    # end
 
   end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'database_cleaner'
+require 'spec_helper'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -40,6 +41,7 @@ RSpec.configure do |config|
   end
 
   config.include FactoryGirl::Syntax::Methods
+  #config.include RequestSpecHelper
   
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
