ebb33d74aeead2ae7a303c7a1d319f1c75608298
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-07-05 22:29:26 -0700

    Added shouda matchers methods to user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.build(:user) }
+  let(:user) { build(:user) }
 
-  it { expect(@user).to respond_to(:email) }
-  it { expect(@user).to respond_to(:name) }
-  it { expect(@user).to respond_to(:password) }
-  it { expect(@user).to respond_to(:password_confirmation) }
-  it { expect(@user).to be_valid }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
+  it { is_expected.to validate_confirmation_of(:password) }
+  it { is_expected.to allow_value("zizo@ribeiro.com").for(:email) }
 
+  
 end
+
+
+
+  # subject { build(:user) }
+  # #subject { FactoryGirl.build(:user) }
+  #
+  # it { expect(subject).to respond_to(:email) }
+  # it { expect(subject).to be_valid }
+
+  # # before { @user = FactoryGirl.build(:user) }
+  # #
+  # # it { expect(@user).to respond_to(:email) }
+  # # it { expect(@user).to respond_to(:name) }
+  # # it { expect(@user).to respond_to(:password) }
+  # # it { expect(@user).to respond_to(:password_confirmation) }
+  # # it { expect(@user).to be_valid }
+
+
