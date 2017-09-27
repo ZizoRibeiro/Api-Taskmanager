e029a339b2c6374a8aff743ae6f9e94c0e70cf0f
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-09-26 23:17:18 -0700

    added update and destroy to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'api_version_constraint'
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
-      resources :users, only: [:show, :create]
+      resources :users, only: [:show, :create, :update, :destroy]
     end
   end
 end
