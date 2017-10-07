cb918a78d722561cd93a8b433423b4ba7a7d715c
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-10-06 20:27:44 -0700

    added test for the task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:task) { build(:task) }
+  context 'When is new' do
+    it { expect(task).not_to be_done } # validations for boolean attributes
+  end
+
+  # association
+  it { is_expected.to belong_to(:user) }
+
+  # validations
+  it { is_expected.to validate_presence_of :title }
+  it { is_expected.to validate_presence_of :user_id }
+
+  # respond to task attributes 
+  it { is_expected.to respond_to(:title) }
+  it { is_expected.to respond_to(:description) }
+  it { is_expected.to respond_to(:deadline) }
+  it { is_expected.to respond_to(:done) }
+  it { is_expected.to respond_to(:user_id) }
 end
