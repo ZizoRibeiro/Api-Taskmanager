3a86acaefea65db3190e5a4d403a150e3a38a95d
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-10-09 23:46:43 -0700

    added task_serializer for the Api V2

---
+++ app/serializers/api/v2/task_serializer.rb
@@ -0,0 +1,14 @@
+class Api::V2::TaskSerializer < ActiveModel::Serializer
+  attributes :id, :title, :description, :done, :deadline,
+             :created_at, :updated_at, :user_id, :short_description, :late?
+
+  belongs_to :user
+
+  def short_description
+    object.description[1..40]
+  end
+
+  def late?
+    Time.current > object.deadline if object.deadline.present?
+  end
+end
