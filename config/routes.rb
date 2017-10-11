9db56c02bcf418dae6872d7c1f3ab452c2f6b2cf
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2017-10-10 19:53:21 -0700

    added devise token auth v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
+ 
   devise_for :user, only: [:sessions], controllers: { sessions: 'api/v1/sessions'}
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
@@ -10,6 +11,8 @@ Rails.application.routes.draw do
       resources :tasks, only: [:index, :show, :create, :update, :destroy]
     end
     namespace :v2, path: '/', constraints: ApiVersionConstraint.new(version: 2, default: true) do
+      mount_devise_token_auth_for 'User', at: 'auth'
+
       resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
       resources :tasks, only: [:index, :show, :create, :update, :destroy]
